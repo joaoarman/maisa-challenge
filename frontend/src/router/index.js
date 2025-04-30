dec92f0a956d3abd5bb8f227e56102681e8b4030
João Vítor Arman <joao.arman@diarioescola.com.br>
2025-04-30 16:10:17 -0300

    Fixing frontend index router file

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
     path: '/alunos',
     name: 'Students',
     component: () => import('../views/Students.vue'),
