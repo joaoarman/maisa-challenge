94ae4def711c0b580a22da6aaabd0e5bfba77311
João Vítor Arman <joaoarman@192.168.0.4>
2025-05-02 09:49:38 -0300

    Fixing .env configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  // Load env file from root directory
+  const rootEnv = loadEnv(mode, path.resolve(__dirname, '..'), '')
+  console.log('rootEnv:', rootEnv)
+  return {
+    plugins: [vue()],
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(`http://localhost:${rootEnv.API_PORT}/api/v1`)
+    }
+  }
 })
